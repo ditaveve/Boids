3b88337f456b420d01d0777028e9483c8c22c09f
ditaveve <diana.angheluta06@gmail.com>
2026-05-10 22:27:40 +0100

    testing
    Removed boid initialization and drawing logic, keeping only mouse interaction.

--- sketch.js
+++ sketch.js
@@ -1,25 +1,8 @@
-const boids = [];
-const nBoids = 400;
-
 function setup() {
-  angleMode(DEGREES);
   createCanvas(400, 400);
-
-  for(let i = 0; i < nBoids; i++){
-    let newBoid = new Boid();
-    newBoid.position = createVector(5 + i*2, 20);
-    boids.push(newBoid);
-  }
+  background(200);
 }
 
 function draw() {
-  background(220);
-
-  let mouseVector = createVector(mouseX, mouseY);
-
-  for(let i = 0; i < nBoids; i++){
-    boids[i].steer(mouseVector, boids, nBoids);
-    boids[i].updatePhysics(deltaTime*0.001);
-    boids[i].drawBoid();
-  }
+  circle(mouseX, mouseY, 20);
 }
